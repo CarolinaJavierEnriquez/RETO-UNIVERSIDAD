b9cb7505eda82a5f284347a5211dde5554e885f7
CarolinaJavierEnriquez <carolinajavierenriquez@gmail.com>
2015-05-20 17:06:20 -0500

    primer efecto jquery del icono ojito de las sección MisPagos

---
+++ jquery.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+	$(".text-contenido-caja3").click(function(event){
+		event.preventDefault();
+		$(".text-icon").toggleClass("hidden");
+		$(".text-hidden").toggleClass("hidden");
+	});
+
+
+});
